980b16accd69e3d2a20aaa17152ba36266467512
adarsh2701 <adarsharyan2701@gmail.com>
2026-06-22 22:58:18 +0530

    add(raresy.hpp): Add raresy.hpp

---
+++ include/raresy/raresy.hpp
@@ -0,0 +1,14 @@
+// Copyright 2026, Adarsh Aryan
+// Licensed under the Apache License, Version 2.0 (http://www.apache.org/licenses/LICENSE-2.0)
+
+#ifndef RARESY_HPP
+#define RARESY_HPP
+
+#pragma once
+
+#if __cplusplus < 202002L
+#   error "raresy requires C++20 or higher"
+#endif
+
+
+#endif // RARESY_HPP
